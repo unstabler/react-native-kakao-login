6870cb2f15723a3e89f79c97e187a601d07df44b
Suhan Moon <leader@trabricks.io>
2020-01-18 01:29:04 +0900

    ios fixed

--- index.js
+++ index.js
@@ -2,15 +2,18 @@ import { NativeModules } from "react-native";
 
 const { ARNKakaoLogin } = NativeModules;
 
-
-export const login = () => {
-    console.log('ARNKakaoLogin', Object.keys(ARNKakaoLogin));
-};
-
+export const login = ARNKakaoLogin.login;
+export const logout = ARNKakaoLogin.logout;
+export const unlink = ARNKakaoLogin.unlink;
+export const getAccessToken = ARNKakaoLogin.getAccessToken;
+export const getProfile = ARNKakaoLogin.getProfile;
 
 const KakaoLogin = {
-    login
+    getAccessToken,
+    getProfile,
+    login,
+    logout,
+    unlink,
 };
-
 
 export default KakaoLogin;
--- ios/ARNKakaoLogin.m
+++ ios/ARNKakaoLogin.m
@@ -10,6 +10,47 @@
 }
 RCT_EXPORT_MODULE(ARNKakaoLogin)
 
+NSObject* handleNullableString(NSString *_Nullable string)
+{
+    return string != nil ? string : [NSNull null];
+}
+
+NSObject* handleKOBoolean(KOOptionalBoolean boolean)
+{
+    switch(boolean){
+        case KOOptionalBooleanTrue : return @(YES);
+        case KOOptionalBooleanFalse: return @(NO);
+        case KOOptionalBooleanNull : return [NSNull null];
+    }
+}
+
+NSObject* handleKOGender(KOUserGender gender)
+{
+    switch(gender){
+        case KOUserGenderNull : return [NSNull null];
+        case KOUserGenderMale : return @"MALE";
+        case KOUserGenderFemale : return @"FEMALE";
+    }
+}
+
+NSObject* handleKOAgeRange(KOUserAgeRange range)
+{
+    switch(range) {
+        case KOUserAgeRangeNull : return [NSNull null];
+        case KOUserAgeRangeType0 : return @"0-9";
+        case KOUserAgeRangeType10 : return @"10-14";
+        case KOUserAgeRangeType15 : return @"15-19";
+        case KOUserAgeRangeType20 : return @"20-29";
+        case KOUserAgeRangeType30 : return @"30-39";
+        case KOUserAgeRangeType40 : return @"40-49";
+        case KOUserAgeRangeType50 : return @"50-59";
+        case KOUserAgeRangeType60 : return @"60-69";
+        case KOUserAgeRangeType70 : return @"70-79";
+        case KOUserAgeRangeType80 : return @"80-89";
+        case KOUserAgeRangeType90 : return @"90-";
+    }
+}
+
 - (BOOL)isLogin
 {
     return [[KOSession sharedSession] isOpen];
@@ -60,7 +101,6 @@ RCT_REMAP_METHOD(login, loginWithResolver: (RCTPromiseResolveBlock)resolve rejec
 }
 
 RCT_REMAP_METHOD(logout, logoutWithResolver: (RCTPromiseResolveBlock)resolve rejecter: (RCTPromiseRejectBlock)reject) {
-    [[KOSession sharedSession] close];
     [[KOSession sharedSession] logoutAndCloseWithCompletionHandler:^(BOOL success, NSError *error) {
         if (!success) {
             reject(@"ARNKakaoLogin", error.userInfo.description, nil);
@@ -70,6 +110,50 @@ RCT_REMAP_METHOD(logout, logoutWithResolver: (RCTPromiseResolveBlock)resolve rej
         }
     }];
 }
+
+RCT_REMAP_METHOD(unlink, unlinkWithResolver: (RCTPromiseResolveBlock)resolve rejecter: (RCTPromiseRejectBlock)reject) {
+    [KOSessionTask unlinkTaskWithCompletionHandler:^(BOOL success, NSError * _Nullable error) {
+        if (!success) {
+            reject(@"ARNKakaoLogin", error.userInfo.description, nil);
+        }
+        else {
+            resolve(@"SUCCESS");
+        }
+    }];
+}
+
+RCT_REMAP_METHOD(getProfile, getProfileWithResolver: (RCTPromiseResolveBlock)resolve rejecter: (RCTPromiseRejectBlock)reject) {
+    [KOSessionTask userMeTaskWithCompletion:^(NSError *error, KOUserMe *me) {
+        if (me) {
+            NSDictionary * result = @{
+                @"ID": handleNullableString(me.ID),
+                @"hasSignedUp": handleKOBoolean(me.hasSignedUp),
+                @"account": @{
+                    @"profileNeedsAgreement": me.account.profileNeedsAgreement ? @YES : @NO,
+                    @"profile": @{
+                        @"nickname": handleNullableString(me.account.profile.nickname),
+                        @"profileImageURL": handleNullableString(me.account.profile.profileImageURL.absoluteString),
+                        @"thumbnailImageURL": handleNullableString(me.account.profile.thumbnailImageURL.absoluteString),
+                    },
+                    @"emailNeedsAgreement": me.account.emailNeedsAgreement ? @YES : @NO,
+                    @"isEmailVerified": handleKOBoolean(me.account.isEmailVerified),
+                    @"email": handleNullableString(me.account.email),
+                    @"ageRangeNeedsAgreement": me.account.ageRangeNeedsAgreement ? @YES : @NO,
+                    @"ageRange": handleKOAgeRange(me.account.ageRange),
+                    @"birthdayNeedsAgreement": me.account.birthdayNeedsAgreement ? @YES : @NO,
+                    @"birthday": handleNullableString(me.account.birthday),
+                    @"genderNeedsAgreement": me.account.genderNeedsAgreement ? @YES : @NO,
+                    @"gender": handleKOGender(me.account.gender)
+                },
+                @"properties": me.properties,
+            };
+            resolve(result);
+        } else {
+            reject(@"ARNKakaoLogin", error.userInfo.description, nil);
+        }
+    }];
+}
+
 
 @end
 
